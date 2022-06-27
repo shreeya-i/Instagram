9bfbeebb8bbfadcbafa81da4c121724ce4d2d3a0
Shreeya Indap <shreeyaindap@fb.com>
2022-06-27 14:40:31 -0700

    log in

--- Instagram/HomeViewController.m
+++ Instagram/HomeViewController.m
@@ -6,6 +6,10 @@
 //
 
 #import "HomeViewController.h"
+#import "AppDelegate.h"
+#import "SceneDelegate.h"
+#import "LoginViewController.h"
+#import "Parse/Parse.h"
 
 @interface HomeViewController ()
 
@@ -18,9 +22,25 @@
     // Do any additional setup after loading the view.
 }
 
-/*
-#pragma mark - Navigation
+- (IBAction)didTapLogout:(id)sender {
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+           if (error) {
+               NSLog(@"Cannot log out");
+           } else {
+               // Success
+               NSLog(@"User logged out successfully.");
+               SceneDelegate *sceneDelegate = (SceneDelegate *)self.view.window.windowScene.delegate;
+               UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+               LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+               sceneDelegate.window.rootViewController = loginViewController;
+           }
+       }];
+}
 
+
+
+
+/*
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
